b0cb587dfd9aaab5346617d69725a779112ff854
AgustinPM <agusax28@gmail.com>
2018-05-29 17:59:20 +0200

    User & Car Models

---
+++ src/main/java/com/atsistemas/cohceuser/model/User.java
@@ -0,0 +1,31 @@
+package com.atsistemas.cohceuser.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idUser;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = Car.FIELD_USER)
+	private List<Car> car;
+	
+}
