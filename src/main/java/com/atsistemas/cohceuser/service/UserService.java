6d1af1e24d41abfe48c177710afccffd7269477c
bonaerges <sclizaran@hotmail.com>
2018-05-29 19:02:03 +0200

    added user , car and userdto

--- src/main/java/com/atsistemas/cohceuser/service/UserService.java
+++ src/main/java/com/atsistemas/cohceuser/service/UserService.java
@@ -2,6 +2,10 @@ package com.atsistemas.cohceuser.service;
 
 import java.util.List;
 
+import com.atsistemas.cohceuser.dto.UserDTO;
+import com.atsistemas.cohceuser.model.Car;
+import com.atsistemas.cohceuser.model.User;
+
 public interface UserService {
 	
 	User create(UserDTO usuario);
