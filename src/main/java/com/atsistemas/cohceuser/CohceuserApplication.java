1bb59b838dc837b3ef7ecdf937820d5e2081e383
Formacion <Formacion@10.11.99.209>
2018-05-29 17:43:34 +0200

    comit incial

---
+++ src/main/java/com/atsistemas/cohceuser/CohceuserApplication.java
@@ -0,0 +1,12 @@
+package com.atsistemas.cohceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohceuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohceuserApplication.class, args);
+	}
+}
